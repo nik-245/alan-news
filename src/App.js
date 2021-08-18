c7007e79c5f0bdf9b77590a952e20a6fa69b072e
nikunj ladva <nikunjladva245@gmail.com>
2021-08-18 21:30:22 +0530

    add the all code regrads the alan

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import React , {useEffect} from 'react';
 import './App.css';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import { useState } from 'react';
+import NewsCards from './component/newscards/NewsCards';
+import useStyles from './style';
+import wordsToNumbers from 'words-to-numbers';
 
 const alankey = '7cdbe3bd8bd21fc4d5f532b79da390c52e956eca572e1d8b807a3e2338fdd0dc/stage';
 function App() {
+    const [newsArticles , setnewsArticles] = useState([]);
+    const [activeArticle , setActiveArticle] = useState(-1);
+    const classes = useStyles();
   useEffect(() => {
      alanBtn({
        key : alankey,
-       onCommand : ({command})=>{
-          if(command === 'testCommand'){
-            alert('this command are done');
+       onCommand : ({command , articles , number})=>{
+          if(command === 'newHeadlines'){ 
+            setnewsArticles(articles);
+            setActiveArticle(-1);
+          }else if(command === 'highlight'){
+              setActiveArticle((prevActiveArticle)=>prevActiveArticle +1 )
+          }else if(command === 'open'){
+            // window.open(articles[number].url , '_blank')
+            const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
           }
        }
      })
   }, [])
   return (
     <>
-      <h1>Alan AI news application</h1>
+      <div className={classes.logoContainer}>
+         <img className={classes.alanLogo} src="https://static8.depositphotos.com/1081417/992/i/950/depositphotos_9927251-stock-photo-good-news.jpg" alt="Alan Ai" />
+      </div>
+      <NewsCards  articles={newsArticles} activeArticle={activeArticle}/>
     </>
   );
 }
---
+++ src/component/newscards/styles.js
@@ -0,0 +1,26 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+
+export default makeStyles({
+    container :{
+      padding:'0 5%',
+      margin: 0,
+      width:'100%',
+    },
+    card: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      width: '100%',
+      height: '45vh',
+      padding: '10%',
+      borderRadius: 10,
+      color: 'white',
+    },
+    infoCard: {
+      display: 'flex', flexDirection: 'column', textAlign: 'center',
+    },
+})
+
+ 
